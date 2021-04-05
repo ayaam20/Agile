2063eb2463a594bd1d1e986f2fe423e58fa7f37f
Ayaam Choudhary <ayaamch23@gmail.com>
2021-04-05 14:17:30 +0530

    pt2

--- Q2.java
+++ Q2.java
@@ -1,25 +1,6 @@
-import java.util.Scanner;
-public class Q2
+public class q2{
+public static void main(String args[])
 {
-	public static void main(String args[])
-	{
-		System.out.print("Enter your Salary: ");
-		Scanner inp = new Scanner(System.in);
-		double sal = inp.nextDouble();
-		if(sal<100000)
-		{
-			sal	= (sal*12*0.15)+(sal*12);
-			System.out.println("Incremented Salary is: " +sal);
-		}
-		else if(100000<=sal & sal<200000)
-		{
-			sal = (sal*12*0.10)+(sal*12);
-			System.out.println("Incremented Salary is: " +sal);
-		}
-		else if(sal>=200000)
-		{
-			sal = (sal*12*0.05)+(sal*12);
-			System.out.println("Incremented Salary is: " +sal);
-		}
-	}	
+System.out.println("Hello World!");
+}
 }
